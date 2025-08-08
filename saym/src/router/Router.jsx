e77585c1bdab108ccd47028122106c498636a1d8
yeeun park <145003970+yengniws@users.noreply.github.com>
2025-08-08 22:23:27 +0900

    Feat : Auth done

--- saym/src/router/Router.jsx
+++ saym/src/router/Router.jsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import Layout from '../layout/Layout';
+import Auth from '../components/Auth/Auth';
 
 export const router = createBrowserRouter([
    {
       path: '/',
       element: <Layout />,
-      children: [{ path: '/login', element: <Login /> }],
+      children: [
+         { path: '/login', element: <Login /> },
+         { path: '/auth', element: <Auth /> },
+      ],
    },
 ]);
