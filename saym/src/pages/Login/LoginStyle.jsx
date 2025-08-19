7c40fdf8e6f89f64c8dfd5c7cd87620f57a90653
yeeun park <145003970+yengniws@users.noreply.github.com>
2025-08-19 16:25:24 +0900

    Design : 로그인 ui 수정

--- saym/src/pages/Login/LoginStyle.jsx
+++ saym/src/pages/Login/LoginStyle.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 // 화면 전체를 감싸는 컨테이너
 export const Container = styled.div`
@@ -6,15 +6,29 @@ export const Container = styled.div`
    flex-direction: column;
    justify-content: center;
    align-items: center;
-   height: 100vh;
+   height: 100dvh;
    padding: 20px;
+`;
+
+const moveUpDown = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px); 
+  }
+  100% {
+    transform: translateY(0);
+  }
 `;
 
 // 프로필 이미지가 들어갈 원
 export const LogoCircle = styled.div`
-   margin-bottom: 150px;
+   margin-bottom: 100px;
    display: flex;
    justify-content: center;
+   width: 350px;
+   animation: ${moveUpDown} 2.5s ease-in-out infinite alternate;
 `;
 
 export const Logoimg = styled.img`
